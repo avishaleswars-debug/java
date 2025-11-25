37d591709b0ba11baff2c1b50e857263c0ec7fe3
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-25 18:28:47 +0330

    working

---
+++ numberproblems/Greatestnum.java
@@ -0,0 +1,13 @@
+package numberproblems;
+
+public class Greatestnum {
+    public static void main(String[] args) {
+        int a=5,b=10;
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
