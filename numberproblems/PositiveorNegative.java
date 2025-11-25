85c499d9d198ba65cac70c94b974782614f20374
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-25 18:19:57 +0330

    working

---
+++ numberproblems/PositiveorNegative.java
@@ -0,0 +1,11 @@
+package numberproblems;
+import java.util.*;
+public class PositiveorNegative {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        String result=n>0?"positive":"negative";
+        System.out.println(result);
+       
+    }
+}
