7a3528a72e80dd0f37711fbf6083f9f1c3bbc229
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-26 19:46:04 +0330

    sunny number

---
+++ numberproblems/Sunnynum.java
@@ -0,0 +1,15 @@
+package numberproblems;
+
+public class Sunnynum {
+    public static void main(String[] args) {
+           int n=48+1;
+    int root=(int) Math.sqrt(n);
+if(n==root*root){
+ System.out.println("its  sunny number");
+}
+else{
+ System.out.println("its not sunny number");
+}
+   
+    }
+}
