e2c6ab75e311d8bba86ddeb5e7f3b2cdffcca02f
avishaleswars-debug <avishaleswars@gmail.com>
2025-11-26 16:31:41 +0330

    Automorphic

---
+++ numberproblems/Automorphic.java
@@ -0,0 +1,13 @@
+public class Automorphic {
+    public static void main(String[] args) {
+        int n=25;
+        int last=n%10;
+double ans=last*last;
+if(n==ans){
+    System.out.println("Automorphic ");
+}
+else{
+        System.out.println("not Automorphic ");
+}
+    }
+}
