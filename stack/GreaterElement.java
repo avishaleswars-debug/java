62c04ccfb9bfd6caa9aa65c5cceab2554a52f665
avishaleswars-debug <avishaleswars@gmail.com>
2026-02-24 20:49:23 +0530

    lc 496

---
+++ stack/GreaterElement.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+public class GreaterElement {
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+        int[] arr={1,2,3,4,5};
+        int[] ans=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+
+    while(!s.isEmpty()&&s.peek()<=arr[i]){
+        s.pop();
+    }
+    if(s.isEmpty())
+    ans[i]=-1;
+   
+else
+    ans[i]=s.peek();
+    s.push(arr[i]);
+
+        }
+        for(int i:ans)System.out.print(i);
+    }
+}
