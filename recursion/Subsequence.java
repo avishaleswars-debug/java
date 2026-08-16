b458e73de08c7108b35f8dc3b781a682de5b38c1
avishaleswars-debug <avishaleswars@gmail.com>
2026-08-16 20:54:07 +0530

    findsubsequence sum

--- recursion/Subsequence.java
+++ recursion/Subsequence.java
@@ -2,18 +2,22 @@ package recursion;
 import java.util.*;
 public class Subsequence {
     static int[] arr={3,1,2};
-    static void solve(int i,ArrayList<Integer> list){
+    static int target=3;
+    static ArrayList<Integer> ans=new ArrayList<>();
+    static void solve(int i,ArrayList<Integer> list,int sum){
         if(i==arr.length){
-System.err.println(list);
+//System.err.println(list);
+if(sum==target)System.out.println(list);
 return;
         }
         list.add(arr[i]);
-        solve(i+1,list);
+        solve(i+1,list,sum+arr[i]);
         list.remove(list.size()-1);
-        solve(i+1,list);
+        solve(i+1,list,sum );
     }
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
-        solve(0,list);
+        int sum=0;
+        solve(0,list,sum);
     }
 }
